1e17b90f9fc581ef9270bb76dbe3c15e454537d7
terencewang101 <149046885+terencewang101@users.noreply.github.com>
2024-02-13 01:57:17 +0800

    Fix computer selection bug and display results after 5 rounds

--- rockpaperscissors.js
+++ rockpaperscissors.js
@@ -9,76 +9,59 @@ function getComputerChoice() {
     } else return "paper";
 }
 
-let playerSelection;
-const computerSelection = getComputerChoice();
 let playerScore = 0;
 let computerScore = 0;
+let roundsPlayed = 0;
+let playerSelection;
+let computerSelection;
 
 function playRound(playerSelection, computerSelection) {
-    // playerSelection = prompt("Choose between rock, paper, or scissors!", "").toLowerCase();
+    computerSelection = getComputerChoice();
+    roundsPlayed++;
+    //playerSelection = prompt("Choose between rock, paper, or scissors!", "").toLowerCase();
     if (playerSelection === "rock" && computerSelection === "scissors" ||
     playerSelection === "scissors" && computerSelection === "paper" ||
     playerSelection === "paper" && computerSelection === "rock") {
         playerScore++;
         alert(`Computer chose ${computerSelection}. You won! 
         Your score is ${playerScore}, computer's score is ${computerScore}`);
-        return "win";
     } else if (playerSelection === "rock" && computerSelection === "paper" || 
     playerSelection === "scissors" && computerSelection === "rock" ||
     playerSelection === "paper" && computerSelection === "scissors") {
         computerScore++;
         alert(`Computer chose ${computerSelection}. You lost! 
         Your score is ${playerScore}, computer's score is ${computerScore}`)
-        return "lose";
     } else {
         alert(`Computer chose ${computerSelection}. It's a tie! 
         Your score is ${playerScore}, computer's score is ${computerScore}`)
-        return "tie";
     }
+    announceResults();
 }
-
-/*function isGameOver() {
-    if (playerScore + computerScore < 5) {
-        return "no";
-    } else {
-        return "yes";
-    }
-}
-*/
 
 const scissors = document.querySelector("#scissors-button")
 scissors.addEventListener("click", () => {
-    playerSelection == "scissors";
-    playRound(playerSelection, computerSelection);
+    playRound("scissors", computerSelection);
 })
 
 const paper = document.querySelector("#paper-button")
 paper.addEventListener("click", () => {
-    playerSelection == "paper";
-    playRound(playerSelection, computerSelection);
+    playRound("paper", computerSelection);
 })
 
 const rock = document.querySelector("#rock-button")
 rock.addEventListener("click", () => {
-    playerSelection == "rock";
-    playRound(playerSelection, computerSelection);
+    playRound("rock", computerSelection);
 })
 
-function game() {
-/*    playRound(playerSelection, computerSelection);
-    playRound(playerSelection, computerSelection);
-    playRound(playerSelection, computerSelection);
-    playRound(playerSelection, computerSelection);
-    playRound(playerSelection, computerSelection);
-*/
-    // Announce final score
-    if (playerScore > computerScore) {
-        alert(`You beat the computer ${playerScore} to ${computerScore}!`)
-    } else if (computerScore > playerScore) {
-        alert(`You lost to the computer ${playerScore} to ${computerScore}!`)
-    } else if (computerScore == playerScore && (computerScore + playerScore !== 0)) {
-        alert(`It's a tie of ${playerScore} to ${computerScore}!`);
-    } else return;
-}
-game();
+function announceResults() {
+    if (roundsPlayed > 5) {
+        if (playerScore > computerScore) {
+            alert(`You beat the computer ${playerScore} to ${computerScore}!`)
+        } else if (computerScore > playerScore) {
+            alert(`You lost to the computer ${playerScore} to ${computerScore}!`)
+        } else if (computerScore == playerScore && (computerScore + playerScore !== 0)) {
+            alert(`It's a tie of ${playerScore} to ${computerScore}!`);
+        } else return;
+    } else;
+};
 
